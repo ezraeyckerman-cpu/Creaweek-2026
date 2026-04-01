0439ed22f64b2325c3626f0e0cc695c36255ba5f
XLegends23 <ezra.eyckerman@student.howest.be>
2026-04-01 18:26:43 +0200

    revert to main scene, also added mines
    yes

--- Assets/Scripts/Multiplayer/PlayerConfigurationManager.cs
+++ Assets/Scripts/Multiplayer/PlayerConfigurationManager.cs
@@ -40,7 +40,7 @@ public class PlayerConfigurationManager : MonoBehaviour
         _playerConfigs[index].IsReady = true;
         if (/*_playerConfigs.Count == maxPlayers &&*/ _playerConfigs.All(p => p.IsReady == true))
         {
-            SceneManager.LoadScene("Ezra");
+            SceneManager.LoadScene("Main");
         }
         Debug.Log($"player {index} ready!");
     }
